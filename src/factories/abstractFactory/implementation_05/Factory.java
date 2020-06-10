407ef18c5a6e6f7ded8ade0d6c2c8f015a5ebc79
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-10 16:35:13 +0200

    adjustments in docs: abstract factory

--- src/factories/abstractFactory/implementation_05/Factory.java
+++ src/factories/abstractFactory/implementation_05/Factory.java
@@ -6,10 +6,12 @@ import factories.abstractFactory.implementation_05.toys.Toy;
 public interface Factory {
 
 	static Toy constructToy(String toyName, ToyType toyType) {
-		if (toyName.matches("[Bb]all"))
+//		if (toyName.matches("[Bb]all"))
+		if (toyName.compareToIgnoreCase("ball")==0)
 			return toyType.getFactory().constructToyBall();
 
-		else if (toyName.matches("[Dd]oll"))
+//		else if (toyName.matches("[Dd]oll"))
+		else if (toyName.compareToIgnoreCase("doll")==0)
 			return toyType.getFactory().constructToyDoll();
 
 		else
